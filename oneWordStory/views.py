ef6e3cbe818511f92f79ec3dc1116fe0861be2e2
Szymon Klus <szymonklus27@gmail.com>
2021-04-06 02:44:34 +0100

    added visit counter

--- oneWordStory/views.py
+++ oneWordStory/views.py
@@ -5,17 +5,45 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from oneWordStory.models import Story, UserProfile, Word
+from datetime import datetime
 
 
 # Create your views here.
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 600:
+        visits = visits + 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
 
 def home(request):
     story_list = Story.objects.order_by('-likes')[:5]
     context_dict = {}
     context_dict['featuredStories'] = story_list
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     
     response = render(request, 'home.html', context=context_dict)
+
     return response
+
 
 def about(request):
     return render(request, 'about.html')
