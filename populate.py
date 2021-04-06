e8d9e621792cb62d6d18815025f8617cab2f179c
Adam Clark <2468460C@student.gla.ac.uk>
2021-04-06 16:31:23 +0100

    population script

--- populate.py
+++ populate.py
@@ -4,6 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','group_project_6A.settings')
 import django
 django.setup()
 from oneWordStory.models import Story, Word, UserProfile
+from django.contrib.auth.models import User
 
 def populate():
     
@@ -15,73 +16,90 @@ def populate():
         {'username': 'Kimberly', 'password': 'kimberlyspassword'},
         {'username': 'Tommy', 'password': 'tommyspassword'}
     ]
+    
+    i=0 
+    userData = {}
+    
+    for user in users:
+        u = add_user(user['username'],user['password'])
+        up = add_userProfile(u)
+        userData[i] = up
+        i+=1
         
     stories = [
-        {'title': 'The Man and the Dog', 'author': users[0], 'likes':5},
-        {'title': 'School Dance', 'author': users[1], 'likes':7},
-        {'title': 'Garden of Goodness', 'author': users[2], 'likes':2},
-        {'title': 'The Pirate Attack', 'author': users[3], 'likes':3},
-        {'title': 'Billy the Snake', 'author': users[4], 'likes':11},
-        {'title': '3 Old Men', 'author': users[5], 'likes':6}
+        {'title': 'The Man and the Dog', 'likes':5, 'author':userData[0]},
+        {'title': 'School Dance', 'likes':7, 'author':userData[1]},
+        {'title': 'Garden of Goodness', 'likes':2, 'author':userData[2]},
+        {'title': 'The Pirate Attack', 'likes':3, 'author':userData[3]},
+        {'title': 'Billy the Snake', 'likes':11, 'author':userData[4]},
+        {'title': '3 Old Men', 'likes':6, 'author':userData[5]}
     ]
+    
+    i=0 
+    storyData = {}
+    
+    for story in stories:
+        s = add_story(story['title'], story['likes'], story['author'])
+        storyData[i] = s
+        i+=1
        
     words = [
-        {'story': stories[0], 'userProfile': users[0], 'content': 'There'},
-        {'story': stories[0], 'userProfile': users[1], 'content': 'once'},
-        {'story': stories[0], 'userProfile': users[0], 'content': 'was'},
-        {'story': stories[0], 'userProfile': users[2], 'content': 'a'},
-        {'story': stories[0], 'userProfile': users[1], 'content': 'cat'},
+        {'story': storyData[0], 'userProfile': userData[0], 'content': 'There'},
+        {'story': storyData[0], 'userProfile': userData[1], 'content': 'once'},
+        {'story': storyData[0], 'userProfile': userData[0], 'content': 'was'},
+        {'story': storyData[0], 'userProfile': userData[2], 'content': 'a'},
+        {'story': storyData[0], 'userProfile': userData[1], 'content': 'cat'},
            
-        {'story': stories[1], 'userProfile': users[1], 'content': 'At'},
-        {'story': stories[1], 'userProfile': users[4], 'content': 'the'},
-        {'story': stories[1], 'userProfile': users[0], 'content': 'dance'},
-        {'story': stories[1], 'userProfile': users[4], 'content': 'we'},
-        {'story': stories[1], 'userProfile': users[1], 'content': 'danced'},
+        {'story': storyData[1], 'userProfile': userData[1], 'content': 'At'},
+        {'story': storyData[1], 'userProfile': userData[4], 'content': 'the'},
+        {'story': storyData[1], 'userProfile': userData[0], 'content': 'dance'},
+        {'story': storyData[1], 'userProfile': userData[4], 'content': 'we'},
+        {'story': storyData[1], 'userProfile': userData[1], 'content': 'danced'},
             
-        {'story': stories[2], 'userProfile': users[2], 'content': 'Garden'},
-        {'story': stories[2], 'userProfile': users[5], 'content': 'Garden'},  
+        {'story': storyData[2], 'userProfile': userData[2], 'content': 'Garden'},
+        {'story': storyData[2], 'userProfile': userData[5], 'content': 'Garden'},  
 
-        {'story': stories[3], 'userProfile': users[3], 'content': 'Pirates'},
-        {'story': stories[3], 'userProfile': users[0], 'content': 'ate'},   
-        {'story': stories[3], 'userProfile': users[3], 'content': 'my'},
-        {'story': stories[3], 'userProfile': users[0], 'content': 'cake'},  
+        {'story': storyData[3], 'userProfile': userData[3], 'content': 'Pirates'},
+        {'story': storyData[3], 'userProfile': userData[0], 'content': 'ate'},   
+        {'story': storyData[3], 'userProfile': userData[3], 'content': 'my'},
+        {'story': storyData[3], 'userProfile': userData[0], 'content': 'cake'},  
         
-        {'story': stories[4], 'userProfile': users[4], 'content': 'Billy'},
-        {'story': stories[4], 'userProfile': users[0], 'content': 'was'},   
-        {'story': stories[4], 'userProfile': users[5], 'content': 'looking'},
-        {'story': stories[4], 'userProfile': users[1], 'content': 'for'},  
-        {'story': stories[4], 'userProfile': users[4], 'content': 'his'},
-        {'story': stories[4], 'userProfile': users[3], 'content': 'friends'},   
-        {'story': stories[4], 'userProfile': users[2], 'content': 'but'},
-        {'story': stories[4], 'userProfile': users[4], 'content': 'died'},  
+        {'story': storyData[4], 'userProfile': userData[4], 'content': 'Billy'},
+        {'story': storyData[4], 'userProfile': userData[0], 'content': 'was'},   
+        {'story': storyData[4], 'userProfile': userData[5], 'content': 'looking'},
+        {'story': storyData[4], 'userProfile': userData[1], 'content': 'for'},  
+        {'story': storyData[4], 'userProfile': userData[4], 'content': 'his'},
+        {'story': storyData[4], 'userProfile': userData[3], 'content': 'friends'},   
+        {'story': storyData[4], 'userProfile': userData[2], 'content': 'but'},
+        {'story': storyData[4], 'userProfile': userData[4], 'content': 'died'},  
         
-        {'story': stories[5], 'userProfile': users[5], 'content': '3'},  
-        {'story': stories[5], 'userProfile': users[0], 'content': 'blind'},
-        {'story': stories[5], 'userProfile': users[4], 'content': 'men'},   
-        {'story': stories[5], 'userProfile': users[5], 'content': 'wearing'},
-        {'story': stories[5], 'userProfile': users[2], 'content': 'pink'},  
+        {'story': storyData[5], 'userProfile': userData[5], 'content': '3'},  
+        {'story': storyData[5], 'userProfile': userData[0], 'content': 'blind'},
+        {'story': storyData[5], 'userProfile': userData[4], 'content': 'men'},   
+        {'story': storyData[5], 'userProfile': userData[5], 'content': 'wearing'},
+        {'story': storyData[5], 'userProfile': userData[2], 'content': 'pink'},  
     ]
-      
-    for user in users:
-        add_user(user['username'],user['password'])
-    for story in stories:
-        add_story(story['title'], story['author'], story['likes'])
+        
+
     for word in words:
         add_word(word['story'], word['userProfile'], word['content'])
     
 def add_user(username,password):
-    u = UserProfile.objects.get_or_create(user.username=username)[0]
-    u.user.password = password
+    u = User.objects.get_or_create(username=username)[0]
+    u.password = password
     u.save()
     return u
-def add_story(title,author,likes):
-    s = Story.objects.get_or_create(title=title)[0]
-    s.author = author
+def add_userProfile(user):
+    up = UserProfile.objects.get_or_create(user=user)[0]
+    up.save()
+    return up
+def add_story(title,likes,author):
+    s = Story.objects.get_or_create(title=title,author=author)[0]
     s.likes = likes
     s.save()
     return s
 def add_word(story,userProfile,content):
-    w = Word.objects.get_or_create()[0]
+    w = Word.objects.get_or_create(story=story,userProfile=userProfile,content=content)[0]
     w.story = story
     w.userProfile = userProfile
     w.content = content
